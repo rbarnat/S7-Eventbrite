a0722821f8e032eaa17ec038d8a1a00741148277
rbarnat <rbarnat@gmail.com>
2020-11-04 10:42:11 +0400

    profil utilisateur

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,20 +4,50 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new()
   end
 
   def create
+    @event = Event.new( start_date: params[:start_date],
+                        duration: params[:duration],
+                        title: params[:title],
+                        description: params[:description],
+                        price: params[:price],
+                        location: params[:location],
+                        event_admin_id: params[:event_admin_id])
+
+    if @event.save # essaie de sauvegarder en base @event
+      # si ça marche, il redirige vers la page d'index du site
+      flash[:success] = "L'event a bien été créé."
+      redirect_to events_path
+    else
+      # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+      flash.now[:danger] = "L'event n'a pas été créé."
+      render :new
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update(post_params)
+    redirect_to event_path
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  def post_params
+    param.require(:event).permit(:start_date, :duration, :title, :description, :price, :location, :event_admin_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,53 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @events = Event.find_by(event_admin_id: params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new( email: params[:email], 
+                      password: params[:password],
+                      password_confirmation: params[:password_confirmation], 
+                      description: params[:description], 
+                      first_name: params[:first_name], 
+                      last_name: params[:last_name])
+
+    if @user.save # essaie de sauvegarder en base @user
+      # si ça marche, il redirige vers la page d'index du site
+      flash[:success] = "L'utilisateur a bien été créé."
+      redirect_to users_path
+    else
+      # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+      flash.now[:danger] = "L'utilisateur n'a pas été créé."
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(post_params)
+    redirect_to user_path
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  def post_params
+    param.require(:user).permit(:email, :password, :password_confirmation, :description, :first_name, :last_name)
   end
 end
