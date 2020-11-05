497df4976b7b44e59b2859383929e6087bc41444
rbarnat <rbarnat@gmail.com>
2020-11-05 16:47:58 +0400

    valid url prod mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => 'event_ually.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'event-ually.herokuapp.com' }
 end
